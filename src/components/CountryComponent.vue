f82b00e0f79cb4aa4f6371b882e9251377115f58
Rune Sostack Clausen <runesostackclausen@gmail.com>
2023-04-04 07:41:35 +0200

    Played around with the flags again, failed

--- src/components/CountryComponent.vue
+++ src/components/CountryComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <tr :class="(visits > 0 ? 'table-success' : 'table-danger')">
-    <td>*FLAG*</td>
+    <td>{{ cc }}</td>
     <td>{{ name }}</td>
     <td>{{ visits }}</td>
     <td>{{ firstVisit == 0 ? '-' : firstVisit }}</td>
@@ -13,21 +13,9 @@ import {defineComponent} from "vue";
 
 export default defineComponent({
   name: "CountryComponent",
-  props: ['name', 'visits', 'cc', 'firstVisit', 'latestVisit']
+  props: ['name', 'cc', 'visits', 'cc', 'firstVisit', 'latestVisit']
 });
 </script>
 
 <style scoped>
-div {
-  margin-bottom: 5px;
-  padding: 5px 15px 5px 15px;
-}
-
-.item {
-  flex: 1 1 0px;
-}
-
-.item-end {
-  text-align:right;
-}
 </style>
